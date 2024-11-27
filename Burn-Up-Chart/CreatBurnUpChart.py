446735473efdf860ddf2da12790b9984c0d861bb
upxul <upxul@student.kit.edu>
2024-11-27 14:23:27 +0100

    This Branch is used to add the Burnup code temporarly to show how it could work. If decided against this way this Branch can be deletet.
    I have Createt a log.csv File for loging of the done estimates at that Date.
    
    I have Createt a Pythons script to make a Burnup plot and save it.

---
+++ Burn-Up-Chart/CreatBurnUpChart.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import csv
+
+max_estimate = 154  #Current total Estimate
+
+with open("Burn-Up-Chart/log.csv", "r") as file:
+    dates = []      # will hold the Dates of the log.csv
+    estimates = []  # will hold the estimates done at the coresbongin date
+
+    # Load the Dates and estimates out of the log.csv
+    csvFile = csv.reader(file)
+    current_estimeate = 0
+    for line in csvFile:
+        dates.append(datetime.datetime.strptime(line[0], '%Y-%m-%d').date())
+        current_estimeate += int(line[1])
+        estimates.append(current_estimeate)
+
+
+    date_pos = np.linspace(1, len(dates), len(dates))   # linear arry for all Dates for Ploting.
+    
+    plt.axis([0,len(dates)+5, 0, max_estimate+5]) # Fixes the viewport for the Plot
+    plt.hlines(max_estimate, 0, len(dates)+5) # Plots the total estimate line (Goal line)
+    plt.plot(date_pos, estimates) # Plots a line with the done estimates
+    plt.plot(date_pos, estimates, 'o') # Plors Dots for the done estimates
+    plt.savefig("Burn-Up-Chart/Brunup-plot.png")
+
