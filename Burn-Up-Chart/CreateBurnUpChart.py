f55825f676693edcc86ef581584c2c428d903b0a
Userfgf <113731092+Userfgf@users.noreply.github.com>
2024-12-08 21:13:49 +0100

    Fixed spelling in CreateBurnUpChart.py

--- Burn-Up-Chart/CreateBurnUpChart.py
+++ Burn-Up-Chart/CreateBurnUpChart.py
@@ -3,24 +3,24 @@ import numpy as np
 import datetime
 import csv
 
-max_estimate = 75  #Current total Estimate
+max_estimate = 75  #Current total estimate
 
 with open("Burn-Up-Chart/log.csv", "r") as file:
     dates = []      # will hold the Dates of the log.csv
-    estimates = []  # will hold the estimates done at the coresbongin date
+    estimates = []  # will hold the estimates done at the corresponding date
 
-    # Load the Dates and estimates out of the log.csv
+    # Load the dates and estimates from the log.csv
     csvFile = csv.reader(file)
-    current_estimeate = 0
+    current_estimate = 0
     for line in csvFile:
         dates.append(datetime.datetime.strptime(line[0], '%Y-%m-%d').date())
-        current_estimeate += int(line[1])
-        estimates.append(current_estimeate)
+        current_estimate += int(line[1])
+        estimates.append(current_estimate)
 
 
-    date_pos = np.linspace(1, len(dates), len(dates))   # linear arry for all Dates for Ploting.
+    date_pos = np.linspace(1, len(dates), len(dates))   # linear array of all dates for ploting.
     
-    plt.axis([0,len(dates)+5, 0, max_estimate+5]) # Fixes the viewport for the Plot
+    plt.axis([0,len(dates)+5, 0, max_estimate+5]) # Fixes the viewport for the plot
     plt.hlines(max_estimate, 0, len(dates)+5) # Plots the total estimate line (Goal line)
 
     plt.title("Burn Up Chart")
@@ -29,5 +29,5 @@ with open("Burn-Up-Chart/log.csv", "r") as file:
 
     plt.plot(date_pos, estimates) # Plots a line with the done estimates
     plt.plot(date_pos, estimates, 'o') # Plors Dots for the done estimates
-    plt.savefig("Burn-Up-Chart/Brunup-plot.png")
+    plt.savefig("Burn-Up-Chart/Burnup-plot.png")
 
