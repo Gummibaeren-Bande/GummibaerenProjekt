f3e5d548be6b113326b1f7c3670f58420a2b7ce9
Jonas Maihöfer <jonas.maihoefer@about-software.de>
2024-12-10 16:30:25 +0100

    style: format backend

--- shared-backend/src/index.ts
+++ shared-backend/src/index.ts
@@ -7,18 +7,18 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: ["http://localhost:8080", "http://localhost:8081"],
-  }
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
-  socket.on('add-team', (name) => {
+  socket.on("add-team", (name) => {
     console.log(`ein Team mit dem Name ${name} wurde erstellt`);
-  })
+  });
 });
 
 httpServer.listen(3000);
-console.log('server running at http://localhost:3000');
+console.log("server running at http://localhost:3000");
