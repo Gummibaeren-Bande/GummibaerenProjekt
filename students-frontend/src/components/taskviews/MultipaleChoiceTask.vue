ce77c49ae0d2cee32da342e8b3ee5efc55b8984e
upxul <upxul@student.kit.edu>
2024-12-27 12:45:04 +0100

    Updated a file import to the new file Name.

--- students-frontend/src/components/taskviews/MultipaleChoiceTask.vue
+++ students-frontend/src/components/taskviews/MultipaleChoiceTask.vue
@@ -8,6 +8,6 @@
   
 <script lang="ts" setup>
 import TaskQuestion from '../taskcomponents/TaskQuestion.vue'
-import TaskMultipleChoice from '../taskcomponents/TaskMultipleChoice.vue';
+import TaskMultipleChoice from '../taskcomponents/TaskMultipleChoiceSction.vue';
 import TaskNumericAnswerbar from '../taskcomponents/TaskNumericAnswerbar.vue';
 </script>
