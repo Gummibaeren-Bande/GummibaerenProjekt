3f4a659d380e9d4b8b3ae9c9cf7a9b66d0453870
felix.agah <113731092+Userfgf@users.noreply.github.com>
2025-01-29 09:07:04 +0100

    fixed hosting

--- teachers-frontend/vite.config.ts
+++ teachers-frontend/vite.config.ts
@@ -3,20 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv'
 
+// Load environment variables from server.env
+dotenv.config({ path: './server.env' })
 // https://vite.dev/config/
 export default defineConfig({
   base: '/panel/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   server: {
     port: 8081,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
