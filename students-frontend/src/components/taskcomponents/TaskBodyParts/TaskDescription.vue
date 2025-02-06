27d4ba2626b00bbdbffb38937d899e300ce88b5e
upxul <upxul@student.kit.edu>
2025-02-06 18:31:13 +0100

    First try on how an imag can look on the Page. I like the css Styling withe the max-wisth:100% the Image will not get Larger as it need sto be. But can adapt to smaller screens (Phone).

--- students-frontend/src/components/taskcomponents/TaskBodyParts/TaskDescription.vue
+++ students-frontend/src/components/taskcomponents/TaskBodyParts/TaskDescription.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="taskDescriptionStyle">
     <ScrollPanel class="taskDescriptionScrollpanel">
-      <p>
+      <img src="https://platform.physik.kit.edu/hedgedoc/uploads/299d27bd-f820-4233-9b44-d57920bf31f3.jpg" alt="Image" class = "Image" />
+      <p ref="test">
         {{ description }}
       </p>
     </ScrollPanel>
@@ -11,17 +12,33 @@
 <script lang="ts">
 import '@/assets/frontend.css'
 import ScrollPanel from 'primevue/scrollpanel'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { marked } from 'marked'
+import { Image } from 'primevue'
 
 export default defineComponent({
   components: {
     ScrollPanel,
+    Image
   },
   props: {
     description: {
       type: String,
       requiered: true,
     },
+  },
+  // All Methods are Temporary at the moment and will maybe be removed.
+  methods: {
+    markdown() {
+      return marked.parse(this.description || '')
+    },
+    picturewidth() {
+      const test = ref<HTMLElement | null>(null)
+      if (test.value) {
+        console.log(test.value.clientWidth)
+        return test.value.clientWidth
+      }
+    }
   },
 })
 </script>
