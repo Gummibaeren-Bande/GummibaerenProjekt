6421df2148d22b924aea0d9fd98a723cab4672de
felix.agah <113731092+Userfgf@users.noreply.github.com>
2025-02-07 12:43:12 +0100

    feat: enhance setup and start scripts for improved readability

--- setup.js
+++ setup.js
@@ -1,16 +1,23 @@
 const { exec } = require("child_process");
 const path = require("path");
 
+// Define an array of subfolder names
 const subfolders = ["shared-backend", "students-frontend", "teachers-frontend"];
 
+// Iterate over each subfolder
 subfolders.forEach((folder) => {
+  // Construct the full path to the subfolder
   const folderPath = path.join(__dirname, folder);
+  // Execute the 'npm install' command in the subfolder to install dependencies
   exec(`cd ${folderPath} && npm install`, (error, stdout, stderr) => {
+    // If there is an error during the execution, log the error and return
     if (error) {
       console.error(`Error installing dependencies in ${folder}:`, error);
       return;
     }
+    // Log the standard output from the command
     console.log(`Output for ${folder}:`, stdout);
+    // Log any errors from the command
     console.error(`Errors for ${folder}:`, stderr);
   });
 });
--- start.js
+++ start.js
@@ -1,7 +1,9 @@
 const { exec } = require("child_process");
 const path = require("path");
+// Import the 'os' module to get information about the operating system
 const os = require("os");
 
+// Define an array of folder objects, each containing a path and a command to execute
 const folders = [
   { path: "shared-backend", command: "npm run start" },
   {
@@ -14,24 +16,34 @@ const folders = [
   },
 ];
 
+// Get the platform of the operating system
 const platform = os.platform();
 console.log("Platform: ", platform);
 
+// Iterate over each folder object
 folders.forEach((folder) => {
+  // Construct the full path to the folder
   const fullPath = path.join(__dirname, folder.path);
+  // Get the command to execute for the folder
   const commands = folder.command;
 
   let command;
+  // Check for operating system and construct the appropriate command
   if (platform === "win32") {
+    // Construct the command to open a new cmd.exe window and execute the commands
     command = `start cmd.exe /K "cd ${fullPath} && ${commands}"`;
     console.log("Command: ", command);
   } else if (platform === "darwin") {
+    // Construct the command to open a new Terminal window on macOS and execute the commands
     command = `osascript -e 'tell application "Terminal" to do script "cd ${fullPath} && ${commands}"'`;
   } else {
+    // Construct the command to open a new terminal window on Linux and execute the commands
     command = `gnome-terminal -- bash -c "cd ${fullPath} && ${commands}; exec bash"`;
   }
 
+  // Execute the constructed command
   exec(command, (error, stdout, stderr) => {
+    // If there is an error during the execution, log the error and return
     if (error) {
       console.error(
         `Error executing command: ${command} in ${folder.path}`,
