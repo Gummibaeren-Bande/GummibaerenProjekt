69a52733ba7b16139e46b6564afef05a72c8709a
Userfgf <upavo@student.kit.edu>
2025-09-01 14:56:50 +0200

    refactor: remove unused prerequisites and trackable task state from Task class

--- shared-backend/src/entities/data/Task.ts
+++ shared-backend/src/entities/data/Task.ts
@@ -1,8 +1,5 @@
 import Exercise from "./exercise/abstract/Exercise";
 import { v4 as uuidv4 } from "uuid";
-import Prerequisite from "./Prerequisite";
-import TrackableTaskState from "../../enums/TrackableTaskState";
-
 /**
  * This class represents a task that contains a list of exercises.
  */
@@ -10,7 +7,6 @@ class Task {
   private readonly id: string;
   private readonly name: string;
   private readonly exercises: Exercise[];
-  private prerequisites: Prerequisite[] = [];
 
   constructor(name: string, exercises: Exercise[]) {
     this.id = uuidv4();
@@ -28,18 +24,6 @@ class Task {
 
   public getExercises(): Exercise[] {
     return this.exercises;
-  }
-
-  public getPrerequisites(): Prerequisite[] {
-    return this.prerequisites;
-  }
-
-  public addPrerequisite(
-    prerequisite: Task,
-    equals: boolean,
-    state: TrackableTaskState
-  ): void {
-    this.prerequisites.push(new Prerequisite(prerequisite, equals, state));
   }
 }
 
